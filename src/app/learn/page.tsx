2529d9fe0236915581260d83b670f2595116297f
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-05-19 01:44:25 +0200

    Add Suspense wrapper to LearnPage component

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,1 +1,10 @@
+import { Suspense } from "react";
+import LearnPageClient from "./LearnPageClient";
 
+export default function LearnPage() {
+  return (
+    <Suspense>
+      <LearnPageClient />
+    </Suspense>
+  );
+}
