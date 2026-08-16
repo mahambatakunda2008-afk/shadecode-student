6c68a789914fed86f8edd6fc4a5c23c3832ea055
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-08-16 08:35:21 +0200

    feat: expose ShadeNet resource primitives

---
+++ src/lib/shadenet/index.ts
@@ -0,0 +1,8 @@
+export * from "./protocol";
+export * from "./resource-transfer";
+export * from "./resource-policy";
+export * from "./discovery";
+export * from "./peer-registry";
+export * from "./signaling";
+export * from "./client";
+export { ShadeNetResourceCache, shadeNetCache } from "./cache";
