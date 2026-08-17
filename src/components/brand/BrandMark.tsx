31474c7ae1cc1390a4f315f505cb709bcbd4c17a
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-08-17 20:47:50 +0200

    brand: use refined transparent student mark component

--- src/components/brand/BrandMark.tsx
+++ src/components/brand/BrandMark.tsx
@@ -7,19 +7,21 @@ export type BrandMarkProps = SVGProps<SVGSVGElement> & {
 export function BrandMark({ title = "Shadecode Student", className, ...props }: BrandMarkProps) {
   return (
     <svg
-      viewBox="0 0 256 256"
+      viewBox="0 0 550 580"
       aria-hidden={title ? undefined : true}
       role={title ? "img" : undefined}
       aria-label={title || undefined}
       className={className}
       focusable="false"
+      preserveAspectRatio="xMidYMid meet"
       {...props}
     >
       {title ? <title>{title}</title> : null}
-      <path
-        fill="currentColor"
-        d="M128 14 207 57v43l-79-43-39 21 118 64v43l-79 43-79-43v-43l79 43 39-21-118-64V57l79 43 39-21-39-21-39 21-40-22z"
-      />
+      <g fill="currentColor">
+        <path d="M72.5 146.5 275.5 24.5 475.5 139.5 474.5 224.5 190.5 162.5 72.5 193.5Z" />
+        <path d="M72.5 193.5 477.5 326.5 477.5 389.5 72.5 266.5Z" />
+        <path d="M72.5 340.5 132.5 372.5 132.5 396.5 272.5 480.5 477.5 389.5 477.5 435.5 278.5 555.5 72.5 436.5Z" />
+      </g>
     </svg>
   );
 }
