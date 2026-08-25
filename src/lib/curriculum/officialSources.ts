6f3e9533b2f901c27f837da7e5bdfe443021c669
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-08-25 19:10:37 +0200

    feat: register official Cambridge syllabus sources

---
+++ src/lib/curriculum/officialSources.ts
@@ -0,0 +1,36 @@
+import type { SyllabusSource } from './syllabus';
+
+export const OFFICIAL_CAMBRIDGE_SOURCES: SyllabusSource[] = [
+  {
+    id: 'cambridge-9702-2025-2027',
+    title: 'Cambridge International AS & A Level Physics 9702 syllabus 2025-2027',
+    url: 'https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-international-as-and-a-level-physics-9702/',
+    examYears: '2025-2027',
+    retrievedAt: '2026-08-25',
+    publisher: 'Cambridge International Education',
+  },
+  {
+    id: 'cambridge-9709-2026-2027',
+    title: 'Cambridge International AS & A Level Mathematics 9709 syllabus 2026-2027',
+    url: 'https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-international-as-and-a-level-mathematics-9709/',
+    examYears: '2026-2027',
+    retrievedAt: '2026-08-25',
+    publisher: 'Cambridge International Education',
+  },
+  {
+    id: 'cambridge-9618-2027-2029',
+    title: 'Cambridge International AS & A Level Computer Science 9618 syllabus 2027-2029',
+    url: 'https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-international-as-and-a-level-computer-science-9618/',
+    examYears: '2027-2029',
+    retrievedAt: '2026-08-25',
+    publisher: 'Cambridge International Education',
+  },
+  {
+    id: 'cambridge-9701-2025-2027',
+    title: 'Cambridge International AS & A Level Chemistry 9701 syllabus 2025-2027',
+    url: 'https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-international-as-and-a-level-chemistry-9701/',
+    examYears: '2025-2027',
+    retrievedAt: '2026-08-25',
+    publisher: 'Cambridge International Education',
+  },
+];
