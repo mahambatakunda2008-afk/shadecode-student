09abbfe23405bf7b164ec2aca75362906f393cad
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-08-28 09:23:39 +0200

    style: format canvas viewport component

--- src/components/studyspace/StudyCanvasViewport.tsx
+++ src/components/studyspace/StudyCanvasViewport.tsx
@@ -6,7 +6,12 @@ import CanvasViewportControls from "./CanvasViewportControls";
 import CanvasZoomControls from "./CanvasZoomControls";
 import { DEFAULT_CANVAS_VIEW, type CanvasView } from "@/lib/studyspace/canvas-view-state";
 
-type Props = { storageKey?: string; height?: number; onChange?: (dataUrl: string) => void; className?: string };
+type Props = {
+  storageKey?: string;
+  height?: number;
+  onChange?: (dataUrl: string) => void;
+  className?: string;
+};
 
 export default function StudyCanvasViewport({ storageKey, height, onChange, className }: Props) {
   const [view, setView] = useState<CanvasView>(DEFAULT_CANVAS_VIEW);
@@ -15,13 +20,25 @@ export default function StudyCanvasViewport({ storageKey, height, onChange, clas
     transformOrigin: "0 0",
     width: "max-content",
   };
+
   return (
     <section className={className} aria-label="Study canvas">
-      <div className="mb-2 flex justify-end"><CanvasZoomControls value={view} onChange={setView} /></div>
+      <div className="mb-2 flex justify-end">
+        <CanvasZoomControls value={view} onChange={setView} />
+      </div>
       <div className="overflow-hidden rounded-xl touch-none">
-        <CanvasViewportControls value={view} onChange={setView} className="relative overflow-hidden">
+        <CanvasViewportControls
+          value={view}
+          onChange={setView}
+          className="relative overflow-hidden"
+        >
           <div data-pan="true" style={canvasStyle}>
-            <StudyCanvas storageKey={storageKey} height={height} zoom={view.zoom} onChange={onChange} />
+            <StudyCanvas
+              storageKey={storageKey}
+              height={height}
+              zoom={view.zoom}
+              onChange={onChange}
+            />
           </div>
         </CanvasViewportControls>
       </div>
