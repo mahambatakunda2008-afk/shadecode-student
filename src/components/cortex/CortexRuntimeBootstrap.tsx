fd361071de68ee0ff8c9dd74f6e313096acb39cf
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-09-04 11:28:33 +0200

    feat: mount local Cortex preparation experience

--- src/components/cortex/CortexRuntimeBootstrap.tsx
+++ src/components/cortex/CortexRuntimeBootstrap.tsx
@@ -2,11 +2,12 @@
 
 import { useEffect } from "react";
 import { registerLocalWebCortexRuntime } from "@/lib/cortex/runtime/registerLocalWebRuntime";
+import CortexDeviceSetup from "@/components/cortex/CortexDeviceSetup";
 
 export default function CortexRuntimeBootstrap() {
   useEffect(() => {
     registerLocalWebCortexRuntime();
   }, []);
 
-  return null;
+  return <CortexDeviceSetup />;
 }
