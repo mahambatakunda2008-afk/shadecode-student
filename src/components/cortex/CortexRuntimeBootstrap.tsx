f449d1282cdb3381306c1ace3877806cc5c71b39
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-09-05 10:55:58 +0200

    feat: mount persistent Cortex learning context

--- src/components/cortex/CortexRuntimeBootstrap.tsx
+++ src/components/cortex/CortexRuntimeBootstrap.tsx
@@ -3,11 +3,17 @@
 import { useEffect } from "react";
 import { registerLocalWebCortexRuntime } from "@/lib/cortex/runtime/registerLocalWebRuntime";
 import CortexDeviceSetup from "@/components/cortex/CortexDeviceSetup";
+import CortexLearningContext from "@/components/cortex/CortexLearningContext";
 
 export default function CortexRuntimeBootstrap() {
   useEffect(() => {
     registerLocalWebCortexRuntime();
   }, []);
 
-  return <CortexDeviceSetup />;
+  return (
+    <>
+      <CortexDeviceSetup />
+      <CortexLearningContext />
+    </>
+  );
 }
