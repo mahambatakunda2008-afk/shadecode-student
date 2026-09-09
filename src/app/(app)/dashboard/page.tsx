f542aa078aa1dcd2f483af7494143dcf79728e59
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-09-09 10:46:28 +0200

    refactor(education): make dashboard family shell the single home surface

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import AcademicExperienceHero from "@/components/academic/AcademicExperienceHero";
-import AcademicExperienceActions from "@/components/academic/AcademicExperienceActions";
 import DashboardWatchdog from "@/components/dashboard/DashboardWatchdog";
 
 export default function Dashboard() {
-  return (
-    <>
-      <AcademicExperienceHero />
-      <AcademicExperienceActions />
-      <DashboardWatchdog />
-    </>
-  );
+  return <DashboardWatchdog />;
 }
