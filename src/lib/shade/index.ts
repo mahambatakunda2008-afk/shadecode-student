b0957137cacf368d407583ec794f07b58ed70e45
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-09-15 14:15:51 +0200

    feat(shade): expose language core API

---
+++ src/lib/shade/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export { lexShade } from "./lexer";
+export { parseShade } from "./parser";
+export { runShade } from "./interpreter";
+
+export const SHADE_LANGUAGE_VERSION = "0.1.0-design-core";
