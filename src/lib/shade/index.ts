913d35f48dfca59cedd1519c30d0947f89c22368
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-09-16 05:12:54 +0200

    feat(shade): export capability policy layer

--- src/lib/shade/index.ts
+++ src/lib/shade/index.ts
@@ -4,6 +4,7 @@ export * from "./project";
 export * from "./execution";
 export * from "./ir";
 export * from "./graph";
+export * from "./capabilities";
 export { lexShade } from "./lexer";
 export { parseShade } from "./parser";
 export { runShade } from "./interpreter";
