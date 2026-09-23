236f0b4c6fdb514de5f69fc824bda26c132cf28c
mahambatakunda2008-afk <mahambatakunda2008@outlook.com>
2026-09-23 22:34:25 +0200

    feat(cortex): move browser local inference into worker

---
+++ src/lib/cortex/webllm.worker.ts
@@ -0,0 +1,9 @@
+/// <reference lib="webworker" />
+
+import { WebWorkerMLCEngineHandler } from "@mlc-ai/web-llm";
+
+const handler = new WebWorkerMLCEngineHandler();
+
+self.onmessage = (event: MessageEvent) => {
+  handler.onmessage(event);
+};
